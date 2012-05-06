34f5a8bb3920ec21ca2b8f8632e4cc9c35628884
Adam Curtis <acurtis@spsu.edu>
2012-05-05 21:01:48 -0400

    documentation touchups

--- src/Control/Cond.hs
+++ src/Control/Cond.hs
@@ -6,13 +6,13 @@ module Control.Cond
        , cond, condPlus
          -- * Conditional operator on categories
        , (?.)
-         -- * Conditional operatior on monoids
+         -- * Conditional operator on monoids
        , (?<>)
          -- * Conditional operator on functions
        , select
          -- * C-style ternary conditional
        , (?)
-         -- *Tony Hoare's conditional choice operator
+         -- *Hoare's conditional choice operator
          -- |The following operators form a ternary conditional of the form
          -- 
          -- > t <| p |> f
@@ -22,8 +22,12 @@ module Control.Cond
          -- the value to the left of the first predicate that succeeds.
          -- 
          -- For more information see 
-         -- <zenzike.com/posts/2011-08-01-the-conditional-choice-operator>
+         -- <http://zenzike.com/posts/2011-08-01-the-conditional-choice-operator>
        , (|>), (<|)
+         -- **Unicode variants
+         -- |Intended to resemble the notation used in Tony Hoare's 
+         -- Communicating Sequential Processes.
+       , (⊳), (⊲)
          -- * Lifted conditional and boolean operators
        , ifM, (<||>), (<&&>), notM, condM, condPlusM
        , guardM, whenM, unlessM 
@@ -34,7 +38,7 @@ import Control.Category
 import Data.Monoid
 import Prelude hiding ((.), id)
 
-infixr  0 <|, |>, ?
+infixr  0 <|, |>, ⊳, ⊲, ?
 infixr  1 ??
 infixr  2 <||>
 infixr  3 <&&>
@@ -46,14 +50,15 @@ if' :: Bool -> a -> a -> a
 if' p t f = if p then t else f
 {-# INLINE if' #-}
 
--- |'if'' with the 'Bool' argument at the end (infix 1).
+-- |'if'' with the 'Bool' argument at the end (infixr 1).
 (??) :: a -> a -> Bool -> a
 (??) t f p = if' p t f 
 {-# INLINE (??) #-}
 
--- |A catamorphism for the Bool type. This is analogous to foldr, maybe, and 
--- either. The first argument is the false case, the second argument is the 
--- true case, and the last argument is the predicate value.
+-- |A catamorphism (aka fold) for the Bool type. This is analogous to 
+-- 'foldr', 'Data.Maybe.maybe', and 'Data.Either.either'. The first argument is 
+-- the false case, the second argument is the true case, and the last argument 
+-- is the predicate value.
 bool :: a -> a -> Bool -> a
 bool f t p = if' p t f
 {-# INLINE bool #-}
@@ -72,6 +77,21 @@ cond ((p,v):ls) = if' p v (cond ls)
 
 -- |Lisp-style conditionals generalized over 'MonadPlus'. If no conditions
 -- match, then the result is 'mzero'. This is a safer variant of 'cond'.
+--
+-- Here's a highly contrived example using 'Data.Maybe.fromMaybe': 
+--
+-- @
+--   signum x = fromMaybe 0 . condPlus $ [(x > 0, 1 ) 
+--                                       ,(x < 0, -1)]
+-- @
+--
+-- Alternatively, you could use the '<|' operator from Hoare's ternary
+-- conditional choice operator, like so:
+--
+-- @
+--   signum x = 0 \<| condPlus [(x > 0, 1 ) 
+--                            ,(x < 0, -1)]
+-- @
 condPlus :: MonadPlus m => [(Bool, a)] -> m a
 condPlus [] = mzero
 condPlus ((p,v):ls) = if' p (return v) (condPlus ls)
@@ -178,3 +198,11 @@ False |> f = Just f
 (<|) :: a -> Maybe a -> a
 t <| Nothing = t
 _ <| Just f  = f
+
+-- |Unicode rebinding of '|>'. 
+(⊲) :: a -> Maybe a -> a
+(⊲) = (<|)
+
+-- |Unicode rebinding of '<|'.
+(⊳) :: Bool -> a -> Maybe a
+(⊳) = (|>)
